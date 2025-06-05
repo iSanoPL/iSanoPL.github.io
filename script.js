776da2a8e3d96a6bf250deb3463e7e75ffb7b378
iSanoPL <pawel@isano.pl>
2025-06-05 22:37:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Wyłączenie menu kontekstowego (prawy klik) na logo
+document.addEventListener("contextmenu", function(e) {
+  if (e.target.tagName.toLowerCase() === 'img' && e.target.getAttribute('src') === 'logo.png') {
+    e.preventDefault();
+  }
+});
